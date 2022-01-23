7a9f73b2cd4ba336322ec7f9e7cf1e45f8af6667
john <jw.rooney@gmail.com>
2022-01-23 18:02:12 +0000

    structure changes

--- InMyStorage/InMyStorage/urls.py
+++ InMyStorage/InMyStorage/urls.py
@@ -18,6 +18,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
-    path('', include('landing.urls')),
 ]
